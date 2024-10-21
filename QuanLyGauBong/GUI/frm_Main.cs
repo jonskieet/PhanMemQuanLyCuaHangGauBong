89a66040cba4890f993ef906ee3453af16461231
QuanHD0610 <142657399+QuanHD0610@users.noreply.github.com>
2024-10-21 13:41:07 +0700

    create main form

--- QuanLyGauBong/GUI/frm_Main.cs
+++ QuanLyGauBong/GUI/frm_Main.cs
@@ -1,15 +1,56 @@
-﻿using System;
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GUI
 {
-    public partial class frm_Main : Form
+    public partial class frm_Main : DevExpress.XtraBars.Ribbon.RibbonForm
     {
         public frm_Main()
         {
             InitializeComponent();
-            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void barButtonItem6_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem7_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem9_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem11_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem12_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem21_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem23_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
         }
     }
 }
